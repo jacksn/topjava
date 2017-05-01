5a8604997b7be9cf0e520d70db68505305caa701
jacksn <gml.jackson@gmail.com>
2017-05-01 12:53:44 +0300

    Change base path

--- src/main/webapp/app/shared/config.ts
+++ src/main/webapp/app/shared/config.ts
@@ -1,13 +1,12 @@
 import {RequestOptions, Headers} from "@angular/http";
 
-export const basePath: string = '/topjava';
+export const basePath: string = '/common';
 export const loginPath: string = "/spring_security_check";
 export const mealPath: string = '/rest/profile/meals';
 export const profilePath: string = '/rest/profile';
 export const registerPath: string = '/register';
 export const usersPath: string = '/rest/admin/users';
 export const i18nPath: string = '/i18n';
-
 
 export const headers: Headers = new Headers({
   'Content-Type': 'application/json'
